a42c74f1719554f0c069b102419d2701d16bb478
Skeleton321 <skeleton_321@163.com>
2022-04-05 16:38:28 +0800

    chore(log): 接收到请求时打印请求信息

--- src/main/kotlin/com/github/fastmirrorserver/interceptor/RequestContextInterceptor.kt
+++ src/main/kotlin/com/github/fastmirrorserver/interceptor/RequestContextInterceptor.kt
@@ -3,6 +3,8 @@ package com.github.fastmirrorserver.interceptor
 import com.github.fastmirrorserver.entity.RequestLimit
 import com.github.fastmirrorserver.exception.Forbidden
 import com.github.fastmirrorserver.service.RequestLimitService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
@@ -11,10 +13,12 @@ import javax.servlet.http.HttpServletResponse
 
 @Component
 class RequestContextInterceptor : HandlerInterceptor {
+    private val log: Logger = LoggerFactory.getLogger(RequestContextInterceptor::class.java)
     @Autowired
     private lateinit var service: RequestLimitService
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         if (!super.preHandle(request, response, handler)) return false
+        log.info("${request.remoteAddr}:${request.remotePort} send a ${request.method} request to ${request.requestURI}")
         val limit = service.get(request.remoteAddr, request.getHeader("x-ratelimit-session"))
         response.setHeader("x-ratelimit-session", limit.token)
         response.setIntHeader("x-ratelimit-limit", RequestLimit.MAX_REQ)
